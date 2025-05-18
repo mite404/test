136a9f30b0873428775c609282211b2d17259271
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-05-17 23:56:01 -0400

    Deleting logos from landing page
    Signed-off-by: Ethan Anderson <150651098+mite404@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    </a>
     <h1>Hello Everyone!!!</h1>
     <p>This paragraph is <i>trash</i> bro...</p>
   </div>
