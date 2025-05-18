eb5c5ff4dc23d230f3ef4e097fa96b8470381c98
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-05-17 23:04:29 -0400

    Quick clean up
    - Decreased font size for `<h1>` tag
    - Swapped logo placements
    - Modified footer `<p>`
    
    Signed-off-by: Ethan Anderson <150651098+mite404@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -5,17 +5,17 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>// We are rebranding the site //</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p>This paragraph stinks!!</p>
+    <p>Actually this paragraph is pretty good!! :)</p>
   </div>
 `
 
