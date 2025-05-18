6ad62b475ad09f0ee9962f69264ee2b7888050f1
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-05-17 23:18:01 -0400

    ethan/landing-page-redesign (#2)
    * Rebranding Landing Page
    
    - Change background color
    - Change font color to 'sand'
    
    Signed-off-by: Ethan Anderson <150651098+mite404@users.noreply.github.com>
    
    * Quick clean up
    
    - Decreased font size for `<h1>` tag
    - Swapped logo placements
    - Modified footer `<p>`
    
    Signed-off-by: Ethan Anderson <150651098+mite404@users.noreply.github.com>
    
    * Deleting Counter button element & `counter.js`
    
    Signed-off-by: Ethan Anderson <150651098+mite404@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Ethan Anderson <150651098+mite404@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,23 +1,16 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Everyone!!!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p>My new paragraph!!!!<br>
-    Look at that!</p>
+    <h1>// We are rebranding the site //</h1>
+    <p>Actually this paragraph is pretty good!! :)</p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
